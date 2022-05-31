d8557ed7961bae2f9f6e8b612c2c30ad24798ac4
Nguyen Thi Yen <nguyenyenbg018@gmail.com>
2022-05-31 15:36:19 +0700

    popup collection home page

--- src/app/components/screens/home/home.component.ts
+++ src/app/components/screens/home/home.component.ts
@@ -1,19 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from 'src/app/services/product.service';
+import { NgbModalConfig, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
+  providers: [NgbModalConfig, NgbModal],
 })
 export class HomeComponent implements OnInit {
   categories: any;
-  constructor(private productService: ProductService) {}
+  constructor(
+    private productService: ProductService,
+    config: NgbModalConfig,
+    private modalService: NgbModal
+  ) {
+    config.backdrop = 'static';
+    config.keyboard = false;
+  }
 
   ngOnInit(): void {
     this.getCategories();
   }
 
+  open(content) {
+    this.modalService.open(content);
+  }
   getCategories() {
     this.productService.getCategories().subscribe((res) => {
       this.categories = res;
